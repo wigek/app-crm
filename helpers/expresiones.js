fcb645f154a8036d742fbbb7460bdfdbf85ff9e6
jaimezzapata <zapataval2304@gmail.com>
2025-10-04 11:42:32 -0500

    Finalización de validación de formulario de registro con expresiones regualres

--- helpers/expresiones.js
+++ helpers/expresiones.js
@@ -1,4 +1,7 @@
 export let expresiones = {
     texto: /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]{5,50}$/,
-    numero: /^\d{5,10}$/
+    numero: /^\d{5,10}$/,
+    telefono: /^3\d{9}$/,
+    correo: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    contrasena: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 }
--- helpers/validarRegistro.js
+++ helpers/validarRegistro.js
@@ -29,6 +29,59 @@ arregloInputs.map((input) => {
                     e.target.classList.remove("correcto")
                 }
                 break
+            case "telefono":
+                if (expresiones.telefono.test(e.target.value)) {
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break;
+            case "correo":
+                if (expresiones.correo.test(e.target.value)) {
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break;
+            case "contrasena":
+                if (expresiones.contrasena.test(e.target.value)) {
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break;
+        }
+    })
+})
+
+let lista = document.querySelector('#genero')
+lista.addEventListener("change", (e) => {
+    if (e.target.value !== "") {
+        e.target.classList.add("correcto")
+        e.target.classList.remove("incorrecto")
+    } else {
+        e.target.classList.add("incorrecto")
+        e.target.classList.remove("correcto")
+    }
+})
+
+let hobbies = document.querySelectorAll("input[name='hobbies']")
+hobbies.forEach((hobbie)=>{
+    hobbie.addEventListener("change", ()=> {
+        let hobbiesCheck = document.querySelectorAll("input[name='hobbies']:checked")
+        let cajaHobbies = document.querySelector(".formulario__grupo--hobbies")
+        if(hobbiesCheck.length > 0){
+            cajaHobbies.classList.add("correcto")
+            cajaHobbies.classList.remove("bg-danger")
+        } else {
+            cajaHobbies.classList.add("bg-danger")
+            cajaHobbies.classList.remove("correcto")
         }
     })
 })
